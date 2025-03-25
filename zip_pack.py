2bc53ecd9ea1adeef4b295abab681b073fdd9b61
merak48763 <99465512+merak48763@users.noreply.github.com>
2025-03-26 03:10:24 +0800

    pack zip tool

---
+++ zip_pack.py
@@ -0,0 +1,18 @@
+import os
+from zipfile import ZipFile
+
+def zipdir(path, zip_filename, *, is_pack=True):
+  with ZipFile(zip_filename, "w") as zipfile:
+    for root, _, files in os.walk(path):
+      for file in files:
+        if is_pack:
+          relpath = os.path.relpath(os.path.join(root, file), path)
+        else:
+          relpath = os.path.relpath(os.path.join(root, file), os.path.join(path, ".."))
+        zipfile.write(os.path.join(root, file), relpath)
+    if is_pack:
+      zipfile.write("LICENSE", "LICENSE")
+
+zipdir("datapack", "dist/MapUtil-dp.zip")
+zipdir("resourcepack", "dist/MapUtil-rp.zip")
+zipdir("mcdoc", "dist/MapUtil-mcdoc.zip", is_pack=False)
