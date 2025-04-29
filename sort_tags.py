c30242926303e5b53f60e0358f81b1280026e954
merak48763 <99465512+merak48763@users.noreply.github.com>
2025-04-30 02:33:21 +0800

    multiversion tag sorter

--- sort_tags.py
+++ sort_tags.py
@@ -1,4 +1,10 @@
-import json
+import json, re
+
+class TagEncoder(json.JSONEncoder):
+  OPTIONAL_ENTRY_PATTERN = re.compile(r"\{\s*\"id\":\s*\"(\w+)\",\s*\"required\":\s*false\s*\}")
+  def encode(self, obj):
+    json_repr = super().encode(obj)
+    return TagEncoder.OPTIONAL_ENTRY_PATTERN.sub(r'{"id": "\g<1>", "required": false}', json_repr)
 
 filename = "datapack/data/mu/tags/" + input("filename: datapack/data/mu/tags/")
 if not filename.endswith(".json"):
@@ -6,7 +12,7 @@ if not filename.endswith(".json"):
 
 with open(filename) as file:
   data = json.load(file)
-data["values"] = sorted(data["values"])
+data["values"] = sorted(data["values"], key=lambda v: v if type(v) is str else v["id"])
 with open(filename, "w") as file:
-  json.dump(data, file, indent=2)
+  file.write(json.dumps(data, indent=2, cls=TagEncoder))
   file.write("\n")
